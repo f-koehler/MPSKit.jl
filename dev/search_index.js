4260f0e236542b99393f91d216e81ae62255994c
Documenter.jl <documenter@juliadocs.github.io>
2022-01-19 14:59:01 +0000

    build based on 239599e

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MPSTools","category":"page"},{"location":"#MPSTools","page":"Home","title":"MPSTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MPSTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MPSTools]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MPSToolkit","category":"page"},{"location":"#MPSToolkit","page":"Home","title":"MPSToolkit","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MPSToolkit.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MPSToolkit]","category":"page"}]
 }
