89da874d4a339a90f86484c8a6339335a9fd400b
Documenter.jl <documenter@juliadocs.github.io>
2022-01-19 11:14:44 +0000

    build based on 3bbda55

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MPSKit","category":"page"},{"location":"#MPSKit","page":"Home","title":"MPSKit","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MPSKit.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MPSKit]","category":"page"}]
+}
